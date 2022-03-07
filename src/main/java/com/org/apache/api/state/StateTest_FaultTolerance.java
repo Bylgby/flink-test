3209d6b8294f5b8bd80de155ff9e6660caa34575
martinyuyy <martinyuyy@gmail.com>
2022-03-07 22:11:08 +0800

    sink

--- src/main/java/com/org/apache/api/state/StateTest_FaultTolerance.java
+++ src/main/java/com/org/apache/api/state/StateTest_FaultTolerance.java
@@ -1,7 +1,10 @@
 package com.org.apache.api.state;
 
 import com.org.apache.beans.SensorReading;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.time.Time;
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
+import org.apache.flink.runtime.executiongraph.restart.RestartStrategy;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.runtime.state.memory.MemoryStateBackend;
 import org.apache.flink.streaming.api.TimeCharacteristic;
@@ -29,12 +32,25 @@ public class StateTest_FaultTolerance {
         // 设置时间属性
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
         // 设置状态后端
-        // 检查点保存在JobManager
+        // 状态后端保存在JobManager
         env.setStateBackend( new MemoryStateBackend());
-        // 检查点保存在文件系统
+        // 状态后端保存在文件系统
         env.setStateBackend( new FsStateBackend("hdfs:///"));
-        // 检查点保存在RocksDb 需要导入jar包
+        // 状态后端保存在RocksDb 需要导入jar包
         env.setStateBackend( new RocksDBStateBackend(""));
+
+        // 设置checkpoint
+        // 检查点间隔时间毫秒
+        env.enableCheckpointing(1000);
+
+        // 检查点高级选项
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
+
+        // 重启策略配置
+        // 固定延迟重启  重启次数， 重启间隔时间
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 1000));
+        // 失败率重启    失败次数，总时间段，每次重启间隔时间，  如下表示10分钟内重启3次，每次启动间隔1分钟
+        env.setRestartStrategy(RestartStrategies.failureRateRestart(3, Time.minutes(10), Time.minutes(1)));
 
 
         DataStreamSource<String> streamSource = env.socketTextStream("localhost", 7777);
